1748f4dac98e5b3261e0359bf70b98024b562a5e
Jack T <jackmt9@gmail.com>
2020-01-21 16:53:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,7 +51,7 @@ def movies_with_director_key(name, movies_collection)
   # movie_with_director_name
   # binding.pry
   count1 = 0
-  puts movie_with_director_name(name, movies_collection[0][])
+  puts movie_with_director_name(name, movies_collection[0])
   
 end
 
